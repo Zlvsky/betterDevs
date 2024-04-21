4bcb4a900c6028b54564b78e4939a152495036d2
zlvsky <zlvsky@icloud.com>
2024-04-21 16:03:15 +0200

    feat: added header, button ui, tailwind-merge and clsx

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/header/Header'
 import BigContainer from '@/components/layout/BigContainer'
 import PageWrap from '@/components/layout/PageWrap'
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
     <PageWrap>
+      <Header />
       <BigContainer>{children}</BigContainer>
     </PageWrap>
   )
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,47 +1,46 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { Button } from '../ui/button/Button'
 
 function Header() {
   return (
-    <nav className="fixed inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-gray-950/90">
-      <div className="mx-auto w-full max-w-7xl px-4">
-        <div className="flex h-14 items-center justify-between">
-          <Link className="flex items-center" href="#">
-            <Image src={'/logo.svg'} alt="BetterDevs" width={300} height={100} />
-            <span className="sr-only">Acme Inc</span>
+    <nav className="mx-auto w-full max-w-7xl px-4 shadow-sm">
+      <div className="flex h-14 items-center justify-between py-10">
+        <Link className="flex items-center" href="#">
+          <Image src={'/logo.svg'} alt="BetterDevs" width={200} height={100} />
+          <span className="sr-only">Acme Inc</span>
+        </Link>
+        <nav className="hidden gap-4 md:flex">
+          <Link
+            className="flex items-center text-sm font-medium transition-colors hover:underline"
+            href="#"
+          >
+            Home
           </Link>
-          <nav className="hidden gap-4 md:flex">
-            <Link
-              className="flex items-center text-sm font-medium transition-colors hover:underline"
-              href="#"
-            >
-              Home
-            </Link>
-            <Link
-              className="flex items-center text-sm font-medium transition-colors hover:underline"
-              href="#"
-            >
-              About
-            </Link>
-            <Link
-              className="flex items-center text-sm font-medium transition-colors hover:underline"
-              href="#"
-            >
-              Services
-            </Link>
-            <Link
-              className="flex items-center text-sm font-medium transition-colors hover:underline"
-              href="#"
-            >
-              Contact
-            </Link>
-          </nav>
-          <div className="flex items-center gap-4">
-            {/* <Button size="sm" variant="outline">
-                Sign in
-              </Button>
-              <Button size="sm">Sign up</Button> */}
-          </div>
+          <Link
+            className="flex items-center text-sm font-medium transition-colors hover:underline"
+            href="#"
+          >
+            About
+          </Link>
+          <Link
+            className="flex items-center text-sm font-medium transition-colors hover:underline"
+            href="#"
+          >
+            Services
+          </Link>
+          <Link
+            className="flex items-center text-sm font-medium transition-colors hover:underline"
+            href="#"
+          >
+            Contact
+          </Link>
+        </nav>
+        <div className="flex items-center gap-4">
+          <Button size="sm" variant="outline">
+            Sign in
+          </Button>
+          <Button size="sm">Sign up</Button>
         </div>
       </div>
     </nav>
