ec8aab1100f44de45b21936e1987a11060300b01
Krzysztof Z <45123514+Zlvsky@users.noreply.github.com>
2024-04-21 14:32:59 +0200

    feat: added github auth, next-auth, sessionwrapper

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,15 @@
+'use client'
+
+import { signIn, useSession } from 'next-auth/react'
+
 export default function Home() {
-  return <></>
+  const { data: session } = useSession()
+  if (session) console.log('logged in')
+
+  return (
+    <>
+      <button onClick={() => signIn('google')}>Sign in with google</button>
+      <button onClick={() => signIn('github')}>Sign in with github</button>
+    </>
+  )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import SessionWrapper from '@/components/auth/SessionWrapper'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -16,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <SessionWrapper>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </SessionWrapper>
   )
 }
